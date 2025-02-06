582d4b7ffd2edce7d7623a2789cded36e92e27e5
Andrey <pikachu_andrey@vk.com>
2025-02-06 12:24:40 +0900

    Text

--- examples/text_png.rs
+++ examples/text_png.rs
@@ -5,41 +5,35 @@ use zeraus::{
     components::{
         layout::{linear::LinearLayout, overlay::OverlayLayout, Direction},
         rectangle::Rectangle,
+        text::Text,
     },
     draw::Draw,
     size::Size,
 };
 
-const WIDTH: usize = 200;
+const WIDTH: usize = 500;
 const HEIGHT: usize = 200;
 
 fn main() {
     let mut canvas = Canvas::new(WIDTH, HEIGHT);
 
-    let rect1 = Rectangle::new()
-        .with_size(20, 20)
-        .foreground_color(0xff_ff0000);
+    let text = Text::new()
+        .with_size(24)
+        .with_font_file("./examples/Ubuntu-Regular.ttf")
+        .unwrap()
+        .with_text("Alright!");
 
-    let rect2 = Rectangle::new()
-        .with_size(20, 20)
-        .foreground_color(0xff_00ff00);
+    let tsize = text.size();
+
+    println!("{:?}", tsize);
 
     let mut layout = LinearLayout::new();
     layout.set_direction(Direction::HORIZONTAL);
-    layout.push(rect1);
+    layout.push(text);
 
-    let mut layout2 = LinearLayout::new();
-    layout2.set_direction(Direction::HORIZONTAL);
-    layout2.set_margin(5, 5, 0, 0);
-    layout2.push(rect2);
+    let (total_width, total_height) = layout.size();
 
-    let mut lay = OverlayLayout::new();
-    lay.push(layout);
-    lay.push(layout2);
-
-    let (total_width, total_height) = lay.size();
-
-    lay.draw(&mut canvas, 0, 0);
+    layout.draw(&mut canvas, 0, 0);
 
     canvas.resize(total_width, total_height);
 
---
+++ src/components/text.rs
@@ -0,0 +1,97 @@
+use std::{cmp::max, io::Read};
+
+use fontdue::{self, FontSettings};
+
+use crate::{draw::Draw, size::Size, Drawable};
+
+#[derive(Default, Clone)]
+pub struct Text {
+    //pub(crate) widget: widget::Widget,
+    pub(crate) color: u32,
+    pub(crate) text: String,
+    pub(crate) size: usize,
+    pub(crate) font: Option<fontdue::Font>,
+}
+
+impl Draw for Text {
+    fn draw(&self, canvas: &mut crate::canvas::Canvas, x: usize, y: usize) {
+        let (mut sx, mut sy) = (0usize, 0usize);
+
+        for i in self.text.chars() {
+            let (metrics, character) = self.font.as_ref().unwrap().rasterize(i, self.size as f32);
+
+            for y in (0..metrics.height) {
+                for x in (0..metrics.width) {
+                    canvas.set_pixel(x, y, self.color * (character[y * metrics.width + x] as u32));
+                }
+            }
+
+            sx += metrics.width;
+        }
+    }
+}
+
+impl Size for Text {
+    fn set_size(&mut self, x: usize, y: usize) {
+        unreachable!();
+    }
+
+    fn size(&self) -> (usize, usize) {
+        let (mut width, mut height) = (0usize, 0usize);
+
+        for i in self.text.chars() {
+            let metrics = self.font.as_ref().unwrap().metrics(i, self.size as f32);
+
+            // TODO: Advanced size calculation
+            width += metrics.width;
+            height = max(height, metrics.height);
+        }
+
+        (width, height)
+    }
+}
+
+impl Drawable for Text {}
+
+impl Text {
+    pub fn new() -> Self {
+        Self {
+            color: 0,
+            text: "".to_string(),
+            size: 18,
+            font: None,
+        }
+    }
+
+    pub fn with_size(self, size: usize) -> Self {
+        let mut text = self;
+        text.size = size;
+        text
+    }
+
+    pub fn with_text(self, text: &str) -> Self {
+        let mut text_obj = self;
+        text_obj.text = text.to_string();
+        text_obj
+    }
+
+    pub fn with_font_file(self, font_path: &str) -> Option<Self> {
+        let mut text_obj = self;
+
+        let mut file = std::fs::File::open(font_path).unwrap();
+        let length = file.metadata().unwrap().len();
+        let mut data = vec![0; length as usize];
+
+        file.read(data.as_mut_slice()).unwrap();
+
+        let font = fontdue::Font::from_bytes(data.as_slice(), FontSettings::default());
+
+        if let Err(e) = font {
+            return None;
+        }
+
+        text_obj.font = Some(font.unwrap());
+
+        Some(text_obj)
+    }
+}

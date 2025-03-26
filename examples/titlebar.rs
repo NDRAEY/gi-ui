69e4c0ec42a1e7acbb1c460390f43c4b5c82530b
Andrey <pikachu_andrey@vk.com>
2025-03-26 16:40:35 +0900

    Additional handlers

--- examples/titlebar.rs
+++ examples/titlebar.rs
@@ -23,24 +23,22 @@ fn main() {
         .with_size(800, HEIGHT)
         .foreground_color(0xff_0000ff);
 
-    let close_button = Touchable::with_listener(
-        Margin::like_args(
-            Circle::new()
-                .with_radius(10)
-                .set_foreground_color(0xff_ff0000),
-            0,
-            0,
-            10,
-            0,
-        ),
-        |e, _, _| {
-            println!("Clicked on close");
+    let close_button = Touchable::new(Margin::like_args(
+        Circle::new()
+            .with_radius(10)
+            .set_foreground_color(0xff_ff0000),
+        0,
+        0,
+        10,
+        0,
+    ))
+    .with_touch_listener(|e, _, _| {
+        println!("Clicked on close");
 
-            let elem: &mut Margin<Circle> = i_am_sure_mut(e);
+        let elem: &mut Margin<Circle> = i_am_sure_mut(e);
 
-            *elem.element_mut() = elem.element_mut().set_foreground_color(0xff_ffffff);
-        },
-    );
+        *elem.element_mut() = elem.element_mut().set_foreground_color(0xff_ffffff);
+    });
 
     let minimize_button = Margin::like_args(
         Circle::new()
@@ -69,15 +67,15 @@ fn main() {
     together.push(maximize_button);
 
     let together = Margin::like_args(together, 15, 15, 15, 15);
-    let mut together = Touchable::with(together);
-
-    together.register_callback(Box::new(|elem: &mut dyn Drawable, x, y| {
-        let el = elem
-            .as_any_mut()
-            .downcast_mut::<Margin<LinearLayout>>()
-            .unwrap();
-        el.element_mut().process_touches(x, y);
-    }));
+    let together =
+        Touchable::new(together).with_touch_listener(
+            |elem: &mut dyn Drawable, x, y| {
+            let el = elem
+                .as_any_mut()
+                .downcast_mut::<Margin<LinearLayout>>()
+                .unwrap();
+            el.element_mut().process_touches(x, y);
+        });
 
     bar.push(rect);
     let clickable_layout = bar.push(together);
--- src/components/touchable.rs
+++ src/components/touchable.rs
@@ -10,6 +10,8 @@ pub type TouchListener = dyn FnMut(&mut dyn Drawable, usize, usize);
 
 pub struct Touchable {
     pub(crate) element: Box<dyn Drawable>,
+    pub(crate) mousedown_listener: Box<TouchListener>,
+    pub(crate) mouseup_listener: Box<TouchListener>,
     pub(crate) touch_listener: Box<TouchListener>,
 }
 
@@ -40,28 +42,51 @@ impl Drawable for Touchable {
 }
 
 impl Touchable {
-    pub fn with(element: impl Drawable + 'static) -> Self {
+    pub fn new(element: impl Drawable + 'static) -> Self {
         Self {
             element: Box::new(element),
             touch_listener: Box::new(|_, _, _| {}),
+            mouseup_listener: Box::new(|_, _, _| {}),
+            mousedown_listener: Box::new(|_, _, _| {}),
         }
     }
 
-    pub fn with_listener(
-        element: impl Drawable + 'static,
+    pub fn with_touch_listener(
+        self,
         listener: fn(&mut dyn Drawable, usize, usize),
     ) -> Self {
-        Self {
-            element: Box::new(element),
-            touch_listener: Box::new(listener),
-        }
+        let mut touchable = self;
+        touchable.touch_listener = Box::new(listener);
+        touchable
     }
 
-    pub fn register_callback(&mut self, f: Box<TouchListener>) {
-        self.touch_listener = f;
+    pub fn with_mouseup_listener(
+        self,
+        listener: fn(&mut dyn Drawable, usize, usize),
+    ) -> Self {
+        let mut touchable = self;
+        touchable.mouseup_listener = Box::new(listener);
+        touchable
+    }
+
+    pub fn with_mousedown_listener(
+        self,
+        listener: fn(&mut dyn Drawable, usize, usize),
+    ) -> Self {
+        let mut touchable = self;
+        touchable.mousedown_listener = Box::new(listener);
+        touchable
     }
 
     pub fn touch(&mut self, x: usize, y: usize) {
         (self.touch_listener)(self.element.as_mut(), x, y);
+    }
+    
+    pub fn mouse_up(&mut self, x: usize, y: usize) {
+        (self.mouseup_listener)(self.element.as_mut(), x, y);
+    }
+    
+    pub fn mouse_down(&mut self, x: usize, y: usize) {
+        (self.mousedown_listener)(self.element.as_mut(), x, y);
     }
 }
